df7c8537c233788cfdbd34810591aef8a590e15d
Sy Tran Dung <sytrancsvn@gmail.com>
2024-04-16 17:59:27 +0700

    add format

--- chunk.h
+++ chunk.h
@@ -3,5 +3,19 @@
 
 #include "common.h"
 
-typedef enum { OP_RETURN } OpCode;
+typedef enum
+{
+  OP_RETURN
+} OpCode;
+
+typedef struct
+{
+  int count;
+  int capacity;
+  uint8_t *code;
+
+} Chunk;
+
+void initChunk (Chunk *chunk);
+void writeChunk (Chunk *chunk, uint8_t byte);
 #endif
